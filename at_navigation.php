8fba9b7290f4792a5dab9864f2691aa4a957fd67
Dan K. <dan@kastorsky.ru>
2014-07-01 13:15:20 +0400

    Update at_navigation.php
    add sorting for 'Placing' and add dropdown ability for dynamic menu

--- at_navigation.php
+++ at_navigation.php
@@ -14,24 +14,38 @@ class AT_Navigation {
 	private $settings = array();
 	private $navigation = array();
 	
+
 	##
 	# HOOKS
 	##
-	
+    public function before_read_file_meta(&$headers) 
+    {
+        $headers['placing'] = 'Placing';
+    }
+
+    //get_page_data(&$data, $page_meta)
+    public function get_page_data(&$data, $page_meta) 
+    {
+        $data['placing'] = isset($page_meta['placing']) ? intval($page_meta['placing']) : 0;
+    }
+
 	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
 	{
-		$navigation = array();
-		
-		foreach ($pages as $page)
-		{
-			if (!$this->at_exclude($page))
-			{
-				$_split = explode('/', substr($page['url'], strlen($this->settings['base_url'])+1));
-				$navigation = array_merge_recursive($navigation, $this->at_recursive($_split, $page, $current_page));
-			}
+		global $config;		
+    	$navigation = array();
+
+        foreach ($pages as $page) {
+        	if (!$this->at_exclude($page)) {
+            	$navigation[$page['placing']] = $page;
+            }
+        }
+		ksort($navigation);
+
+		foreach ($navigation as $page) {
+            $_split = explode('/', substr($page['url'], strlen($this->settings['base_url']) + 1));
+            $navigation = array_merge_recursive($navigation, $this->at_recursive($_split, $page, $current_page));
 		}
-		
-		array_multisort($navigation);
+
 		$this->navigation = $navigation;
 	}
 	
@@ -62,38 +76,48 @@ class AT_Navigation {
 	##
 	# HELPER
 	##
-	
 	private function at_build_navigation($navigation = array(), $start = false)
 	{
+
 		$id = $start ? $this->settings['at_navigation']['id'] : '';
 		$class = $start ? $this->settings['at_navigation']['class'] : '';
 		$class_li = $this->settings['at_navigation']['class_li'];
+		$class_lich = 'dropdown';
 		$class_a = $this->settings['at_navigation']['class_a'];
 		$child = '';
-		$ul = $start ? '<ul id="%s" class="%s">%s</ul>' : '<ul>%s</ul>';
+		$ul = $start ? '<ul id="%s" class="%s">%s</ul>' : '<ul class="dropdown-menu">%s</ul>';
 		
 		if (isset($navigation['_child']))
 		{
+			$class_a = 'dropdown-toggle';
+			$class_li = $dataToggle = $dataHover = 'dropdown';
+			$dataDelay = 0; $dataCloseOthers = 'false';
 			$_child = $navigation['_child'];
-			array_multisort($_child);
-			
+			//array_multisort($_child); !!!!!
 			foreach ($_child as $c)
 			{
 				$child .= $this->at_build_navigation($c);
 			}
-			
 			$child = $start ? sprintf($ul, $id, $class, $child) : sprintf($ul, $child);
+			$strin = '<li class="%1$s %5$s"><a href="%2$s" class="%1$s %6$s" data-toggle="%7$s" data-hover="%8$s" data-delay="%9$s" data-close-others="%10$s" title="%3$s">%3$s</a>%4$s</li>';
+		} else {
+			$class_a = $class_li = $dataToggle = $dataHover = $dataDelay = $dataCloseOthers = '';
+			$strin = '<li class="%1$s %5$s"><a href="%2$s" class="%1$s %6$s" title="%3$s">%3$s</a>%4$s</li>';
 		}
-		
+
 		$li = isset($navigation['title'])
 			? sprintf(
-				'<li class="%1$s %5$s"><a href="%2$s" class="%1$s %6$s" title="%3$s">%3$s</a>%4$s</li>',
+				$strin,
 				$navigation['class'],
 				$navigation['url'],
 				$navigation['title'],
 				$child,
 				$class_li,
-				$class_a
+				$class_a,
+				$dataToggle,
+				$dataHover,
+				$dataDelay,
+				$dataCloseOthers
 			)
 			: $child;
 		
@@ -135,7 +159,7 @@ class AT_Navigation {
 	{
 		$activeClass = (isset($this->settings['at_navigation']['activeClass'])) ? $this->settings['at_navigation']['activeClass'] : 'is-active';
 		if (count($split) == 1)
-		{			
+		{
 			$is_index = ($split[0] == '') ? true : false;
 			$ret = array(
 				'title'			=> $page['title'],
@@ -144,8 +168,12 @@ class AT_Navigation {
 			);
 			
 			$split0 = ($split[0] == '') ? '_index' : $split[0];
-			return array('_child' => array($split0 => $ret));
-			return $is_index ? $ret : array('_child' => array($split[0] => $ret));
+
+			if($is_index == 'true') {
+				$menuArray = array('_child' => array($split[0] => $ret));
+			} else {
+				$menuArray = array('_child' => array($split0 => $ret));
+			}
 		}
 		else
 		{
@@ -156,8 +184,9 @@ class AT_Navigation {
 			}
 			
 			$first = array_shift($split);
-			return array('_child' => array($first => $this->at_recursive($split, $page, $current_page)));
+			$menuArray = array('_child' => array($first => $this->at_recursive($split, $page, $current_page)));
 		}
+		return $menuArray;
 	}
 }
 ?>
